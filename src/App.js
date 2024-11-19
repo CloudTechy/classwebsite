59ef5f90ebc76044d12c4ad992186e2f630cb0a5
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-11-19 17:35:43 +0100

    edited message

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to official NIBCODE school website! <br/>This is a test to see if the website test server is working
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://community-development.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
